9373716151519ece359bc5ce608c6c174a638e5f
Faycal TOURE <faycal.toure>
2021-12-23 23:40:42 +0100

    :construction_worker: update array

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", () => {
 // vars
 let markerArray = [];
 
-let flightPlanCoordinates,
+let flightPlanCoordinates = [],
     flightPath,
     flightPathStatus = false,
     sideBar = document.getElementById('sidebar'),
@@ -13,7 +13,7 @@ let flightPlanCoordinates,
 
 sideBar.hidden = true;
 
-function toggleSideBar(){
+function toggleSideBar() {
     return sideBar.hidden = !sideBar.hidden;
 }
 
@@ -21,46 +21,28 @@ function toggleSideBar(){
 function initMap(listener) {
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 6,
-        center: { lat: 46.232192999999995, lng: 2.209666999999996 },
+        center: {lat: 46.232192999999995, lng: 2.209666999999996},
     });
 
     const onChangeHandler = function () {
+        if (flightPathStatus)
+            removeLine();
 
-        if(flightPathStatus)
-            flightPath.setMap(null);
-
-        let start = returnMarkersIndex(markerArray, 1);
-        let end = returnMarkersIndex(markerArray, 2);
-
-        flightPlanCoordinates = [
-            { lat: start.getPosition().lat(),
-                lng: start.getPosition().lng()
-            },
-            { lat: end.getPosition().lat(),
-                lng: end.getPosition().lng()
-            },
-        ];
-
-        flightPath = new google.maps.Polyline({
-            path: flightPlanCoordinates,
-            geodesic: true,
-            strokeColor: "#FF0000",
-            strokeOpacity: 1.0,
-            strokeWeight: 2,
-        });
+        if(!flightPathStatus){
+            drawFlightPlan();
+        }
 
         addLine(flightPath, map);
         toggleSideBar();
-        flightPathStatus = true;
     };
 
     map.addListener("click", (e) => {
         // We limit to 2 markers
-        if(getMarkersLength() < 2){
+        if (getMarkersLength() < 2) {
             addMarker(e.latLng, map);
         }
         // We draw positions
-        if(getMarkersLength() === 2){
+        if (getMarkersLength() >= 2) {
             onChangeHandler();
         }
     });
@@ -73,43 +55,49 @@ function addMarker(latLng, map) {
     let marker = new google.maps.Marker({
         position: latLng,
         map: map,
-    })
+    });
 
     google.maps.event.addListener(marker, 'click', function () {
         deleteMarkers(marker);
+        updatePaneHtml(marker);
+
         if(removeMarkersItem(marker)){
-            if(getMarkersLength() >= 1)
-                removeLine();
-                sideBar.hidden = true;
+            removeLine();
+            hideMarkers(marker);
             flightPathStatus = false;
+        }
+        if(!getMarkersLength()){
+            flightPlanCoordinates = [];
+            updatePaneHtml(marker);
+            toggleSideBar();
         }
     });
 
     addMarkersItem(marker);
-
-    let template = `
-            <li> <strong>Position ${getMarkersLength()}</strong><br> Lat : ${ marker.getPosition().lat() }, Long ${ marker.getPosition().lng() }</li> <br>
-        `;
-
-    paneList.innerHTML += template;
+    createPaneHtml(marker);
 }
 
 // marker Array
-function getMarkersLength(){
+function getMarkers() {
+    return markerArray;
+}
+
+function getMarkersLength() {
     return markerArray.length;
 }
 
-function removeMarkersItem(marker){
-    return markerArray = markerArray.splice(markerArray.indexOf(marker), 1) || null;
+function removeMarkersItem(marker) {
+    let index = markerArray.indexOf(marker);
+    return markerArray = markerArray.slice(index, (index + 1)) || null;
 }
 
-function addMarkersItem(marker){
+function addMarkersItem(marker) {
     markerArray.push(marker);
 }
 
-function returnMarkersIndex(markersArray, arg){
-    if(Array.isArray(markersArray)
-        && markersArray.length > 0){
+function returnMarkersIndex(markersArray, arg) {
+    if (Array.isArray(markersArray)
+        && markersArray.length > 0) {
         return markersArray[(arg - 1)] || null
     }
 }
@@ -121,7 +109,28 @@ function hideMarkers(marker) {
 
 function deleteMarkers(marker) {
     hideMarkers(marker);
-    paneList.innerHTML = '';
+}
+
+function createPaneHtml(marker) {
+    let template = `
+            <li class="my-5" id="${getMarkersLength()}" data-identifier="pan-${getMarkersLength()}"> 
+                <strong>Position ${getMarkersLength()}</strong><br>Lat : ${marker.getPosition().lat()}, Long : ${marker.getPosition().lng()}
+            <br></li>
+        `;
+    paneList.innerHTML += template;
+}
+
+function updatePaneHtml(marker) {
+    let idIndex = (getMarkers().indexOf(marker) + 1);
+    for (let i = 0; i < paneList.children.length; i++){
+        if(parseInt(paneList.children[i].id) === idIndex){
+            document.getElementById(idIndex).remove();
+        }
+    }
+    // drop paneList if no marker
+    if (!getMarkersLength()) {
+        paneList.innerHTML = '';
+    }
 }
 
 // lines
@@ -131,5 +140,30 @@ function addLine(flightPath, map) {
 
 function removeLine() {
     flightPath.setMap(null);
-    markerArray = [];
+}
+
+
+
+// draw
+function drawFlightPlan(){
+    let drawElements = getMarkers();
+
+    for (let i = 0; i < drawElements.length; i++){
+        flightPlanCoordinates.push(
+            {
+                lat: drawElements[i].getPosition().lat(),
+                lng: drawElements[i].getPosition().lng()
+            },
+        );
+    }
+
+    flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+    });
+
+    flightPathStatus = true;
 }
